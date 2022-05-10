73036fde0886277b1ee96cf0d6a0eeb809ae92b6
Alfin <m.alfin.syahruddin@gmail.com>
2022-05-10 15:41:03 +0700

    Initial Commit

---
+++ Shared/IndomiekuApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndomiekuApp.swift
+//  Shared
+//
+//  Created by Alfin on 10/05/22.
+//
+
+import SwiftUI
+
+@main
+struct IndomiekuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
